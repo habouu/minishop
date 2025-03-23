433f39e724f4a9f480ce8bc446233b26dd83a31a
habouu <ummhabiabh-ouattara@outlook.com>
2025-03-23 19:46:00 +0100

    change password

--- src/Controller/ProfileController.php
+++ src/Controller/ProfileController.php
@@ -2,8 +2,13 @@
 
 namespace App\Controller;
 
+use App\Form\PasswordUserType;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+use Symfony\Component\PasswordHasher\PasswordHasherInterface;
 use Symfony\Component\Routing\Attribute\Route;
 
 final class ProfileController extends AbstractController
@@ -12,5 +17,24 @@ final class ProfileController extends AbstractController
     public function index(): Response
     {
         return $this->render('profile/index.html.twig');
+    }
+
+    #[Route('/profile/change-password', name: 'app_profile_change_password')]
+    public function password(Request $request, UserPasswordHasherInterface $passwordHasher, EntityManagerInterface $entityManager): Response
+    {
+        $user = $this->getUser();
+        $form = $this->createForm(PasswordUserType::class, $user, ['passwordHasher' => $passwordHasher]);
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager->flush();
+            $this->addFlash(
+                'success',
+                'Your password have been updated'
+            );
+        }
+
+        return $this->render('profile/password.html.twig', [
+            'form' => $form
+        ]);
     }
 }
---
+++ src/Form/PasswordUserType.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\User;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Validator\Constraints\Length;
+use Symfony\Component\Validator\Constraints\NotNull;
+use Symfony\Component\Validator\Constraints\NotBlank;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
+use Symfony\Component\Form\FormError;
+use Symfony\Component\Form\FormEvent;
+use Symfony\Component\Form\FormEvents;
+
+class PasswordUserType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('actualPassword', PasswordType::class, [
+                'mapped' => false
+            ])
+            ->add('plainPassword', RepeatedType::class, [
+                'type' => PasswordType::class,
+                'first_options'  => ['label' => 'Password', 'hash_property_path' => 'password'],
+                'second_options' => ['label' => 'Repeat Password'],
+                'mapped' => false,
+                'constraints' => [
+                    new NotNull(),
+                    new NotBlank(),
+                    new Length(['min' => 4, 'max' => 12])
+                ]
+            ])
+            ->add('submit', SubmitType::class, [
+                'label' => 'Change password',
+                'attr' => [
+                    'class' => 'btn btn-success'
+                ]
+            ])
+            ->addEventListener(FormEvents::SUBMIT, function (FormEvent $event) {
+                $form = $event->getForm();
+                $user = $form->getConfig()->getOptions()['data'];
+                $passwordHasher = $form->getConfig()->getOptions()['passwordHasher'];
+                
+                $isValid = $passwordHasher->isPasswordValid(
+                    $user,
+                    $form->get('actualPassword')->getData()
+                );
+
+                if (!$isValid) {
+                    $form->get('actualPassword')->addError(new FormError('Your current password is not correct'));
+                }
+            })
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => User::class,
+            'passwordHasher' => null
+        ]);
+    }
+}
